4babf2096b5f50c84198cee993b26e2a0315513c
Luis Bazán <luis@bazan.pe>
2022-05-29 11:25:22 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./main.css";
+
+import HomePage from "./pages/Home/HomePage";
+
+function App() {
+  return (
+    <div>
+      <img src="/background.png" alt="" id="background" />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <div>
+      <h1 id="name">Luis Bazán</h1>
+      <img src="/img/profile.jpg" alt="Profile" />
+    </div>
+  );
+}
+
+export default HomePage;
