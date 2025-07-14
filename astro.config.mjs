cb320eb57b7e5f70d1a7fecdee285d73c487c467
Luis Bazan <me@luisb.space>
2025-07-13 23:26:16 -0500

    Update project to preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import svgr from "@svgr/rollup";
 
-import react from "@astrojs/react";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: "https://luisbazandev.github.io"
+  site: "https://luisbazandev.github.io",
+  integrations: [tailwind(), preact({ compat: true })],
+  vite: {
+    plugins: [svgr()],
+    resolve: {
+      alias: {
+        "@assets": new URL("./src/assets", import.meta.url).pathname,
+      },
+    },
+  },
+  output: "static",
+  compressHTML: true,
 });
